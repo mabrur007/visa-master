9ccd24773f24a834710db96a55332f1ca8d6774b
mabrur007 <mirmd.mabrur@gmail.com>
2022-04-18 14:48:33 +0600

    responsive navbar added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,12 +1,25 @@
 import React from 'react';
-// import { Container, Navbar, Nav } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
         <header>
-            <h2 className='text-center'>This is a header</h2>
+            <Navbar collapseOnSelect className='bg' expand="lg" sticky="top" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                    Visa Master
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>    
+            </Navbar>
         </header>
     );
 };
