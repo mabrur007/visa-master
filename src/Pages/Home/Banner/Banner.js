5389fe73cb08fd532d77f1eb298586025ddac597
mabrur007 <mirmd.mabrur@gmail.com>
2022-04-18 16:22:48 +0600

    banner section added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is banner section</h2>
+        <div className='container'>
+            <div className="banner-area">
+                <div className="content-area">
+                    <h1>
+                        Welcome to <span>Visa Master!</span>
+                    </h1>
+                    <p>
+                        Hi! This is <b>Jonathan Lukeman</b> . I am a professional lawyer with experience of over 10 years. I have been successfully working in this field with the success rate of hundred in hundred. Imigration visa, student visa, non-imigrant visa and a lot of services are provided by me. If you feel that you need any kind of law support, I am the right person you could think about. Just do take an appointment and we will solve it together.
+                        
+                    </p>
+                    <Link to='/about'>
+                        <button className="content-btn">Appointment</button>
+                    </Link>
+                </div>
+                <div className="img-area">
+                    <img src="lawyer.png" alt="lawyer-img" />
+                </div>
+            </div>
         </div>
     );
 };
